25f2f136ca9f93cd04ea913ac2aae9292dd7bd2f
Jun0613-spec <kumo5110@gmail.com>
2023-06-08 15:54:42 +0100

    fixed price

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -38,7 +38,7 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
         className="
       flex flex-row items-center gap-1 p-4"
       >
-        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="text-2xl font-semibold">£{price}</div>
         <div className="font-light text-neutral-600">night</div>
       </div>
       <hr />
