407f8d48739fe7ba333f26714c353dea4395c0cb
Jun0613-spec <kumo5110@gmail.com>
2023-06-08 13:41:01 +0100

    first commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      onClick={() => router.push("/")}
+      className="hidden md:block cursor-pointer"
+      src="/images/Logo.png"
+      height="100"
+      width="100"
+      alt="Logo"
+    />
+  );
+};
+
+export default Logo;
